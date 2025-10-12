2b79eaf251c45997f4fef2ca90954b65f6bf6938
SadBag-dev <denton.piper@gmail.com>
2025-10-12 18:58:53 -0400

    Created player movement with sprint
    player movement controller made with rigidbody using new input system

--- Assets/Code/Scripts/PlayerController.cs
+++ Assets/Code/Scripts/PlayerController.cs
@@ -1,6 +1,37 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerController
+public class PlayerController:MonoBehaviour
 {
-    
+    InputAction moveAction;
+    InputAction sprintAction;
+    public Rigidbody2D player;
+    [SerializeField] private int playerSpeed;
+    [SerializeField] private int playerSprintSpeed;
+
+
+    private void Start()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+        sprintAction = InputSystem.actions.FindAction("Sprint");
+    }
+
+    void FixedUpdate()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        Vector2 moveDirection = moveAction.ReadValue<Vector2>();
+
+        player.linearVelocity = moveDirection * playerSpeed;
+        Debug.Log(player.linearVelocity);
+
+        if (sprintAction.IsPressed())
+        {
+            playerSpeed = playerSprintSpeed;
+        }
+    }
+
 }
