5093f78a9c99d3d1e8fea495a87e53839f998b6a
SadBag-dev <denton.piper@gmail.com>
2025-10-12 19:48:31 -0400

    Sneaking added
    sneaking added
    
    added playerWalkSpeed instead of just playerSpeed for everything
    
    playerSpeed is now set by playerWalkSpeed, playerSprintSpeed, and playerSneakSpeed

--- Assets/Code/Scripts/PlayerController.cs
+++ Assets/Code/Scripts/PlayerController.cs
@@ -5,33 +5,56 @@ public class PlayerController:MonoBehaviour
 {
     InputAction moveAction;
     InputAction sprintAction;
+    InputAction sneakAction;
     public Rigidbody2D player;
-    [SerializeField] private int playerSpeed;
-    [SerializeField] private int playerSprintSpeed;
+    [SerializeField] private float playerWalkSpeed;
+    [SerializeField] private float playerSprintSpeed;
+    [SerializeField] private float playerSneakSpeed;
+    private float playerSpeed;
+    private bool isSprinting;
+    public bool isSneaking;
+    
 
 
     private void Start()
     {
+        playerSpeed = playerWalkSpeed;
         moveAction = InputSystem.actions.FindAction("Move");
         sprintAction = InputSystem.actions.FindAction("Sprint");
+        sneakAction = InputSystem.actions.FindAction("Sneak");
     }
 
     void FixedUpdate()
     {
         PlayerMovement();
+        Debug.Log(isSprinting);
+
     }
 
     void PlayerMovement()
     {
         Vector2 moveDirection = moveAction.ReadValue<Vector2>();
-
         player.linearVelocity = moveDirection * playerSpeed;
-        Debug.Log(player.linearVelocity);
 
         if (sprintAction.IsPressed())
         {
+            isSprinting = true;
             playerSpeed = playerSprintSpeed;
+        }else
+        {
+            isSprinting = false;
+            playerSpeed = playerWalkSpeed;
         }
+
+        if (sneakAction.IsPressed() && isSprinting == false)
+        {
+            isSneaking = true;
+            playerSpeed = playerSneakSpeed;
+        } else
+        {
+            isSneaking = false;
+        }
+
     }
 
 }
